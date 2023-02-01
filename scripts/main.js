19010bafe15a75012db5f9825809ea2118a59a33
Joakim Tveter <joakim@tveter.net>
2023-02-02 00:04:53 +0100

    Started homepage

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
-const logoutButton = document.getElementById('logout-button');
-
+/**
+ * Logs out the user and redirects to login page
+ * @returns {void} Delete user object from local storage and redirect to login.html
+ */
 function logOut() {
     localStorage.removeItem('user');
     window.location.pathname = 'login.html';
 }
 
+const logoutButton = document.getElementById('logout-button');
 logoutButton.addEventListener('click', logOut);
