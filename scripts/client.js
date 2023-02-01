2ca8b7a8758b129ace9963997a84eb7f4de8e485
Joakim Tveter <joakim@tveter.net>
2023-02-02 00:03:35 +0100

    Added Get Posts

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,5 @@
+import { showToast } from './utils/toast.js';
+
 const baseUrl = 'https://api.noroff.dev/api/v1/social';
 let loggedInUser;
 
@@ -12,10 +14,20 @@ function getAccessToken() {
     return loggedInUser?.accessToken;
 }
 
+/**
+ * Function to check if user is logged in.
+ * @returns {boolean} true if user is logged in, false if not
+ */
 function isLoggedIn() {
     return !!getAccessToken();
 }
 
+/**
+ * Logs in user and sets userobjet in localStorage
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<void>} Sets user in localStorage and redirects to index.html
+ */
 async function logIn(email, password) {
     try {
         const response = await fetch(baseUrl + '/auth/login', {
@@ -36,10 +48,17 @@ async function logIn(email, password) {
         window.location.pathname = 'index.html';
     } catch (error) {
         console.error(error);
-        // showToast('error', error);
+        showToast('error', error);
     }
 }
 
+/**
+ * Registers a new user and redirects to login.html
+ * @param {string} name
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise<void>} Redirects to login.html
+ */
 async function register(name, email, password) {
     try {
         const response = await fetch(baseUrl + '/auth/register', {
@@ -54,13 +73,82 @@ async function register(name, email, password) {
             throw new Error(`${error.statusCode} ${error.status} - ${error.errors[0].message}`);
         }
         const data = await response.json();
-        console.log(data);
-        // showToast('success', ` ${data.name} have successfully registered`);
+        showToast('success', `${data.name} have successfully registered`);
         window.location.pathname = 'login.html';
     } catch (error) {
         console.error(error);
-        // showToast('error', error);
+        showToast('error', error);
     }
 }
 
-export { logIn, register, isLoggedIn, getAccessToken, loggedInUser };
+// TODO: Add typedefs to return
+/**
+ * Gets posts from API
+ * @param {string} [sort] - Sorts posts by either created or title (created/title)
+ * @param {string} [sortOrder] - Order posts either ascending or descending (asc/desc)
+ * @param {number} [limit] - Limits the number of posts returned
+ * @param {number} [offset] - Offset the number of posts returned, not paged
+ * @param {string} [tag] - Filter posts by tag
+ * @param {boolean} [author] - Include author in response
+ * @param {boolean} [reactions] - Include reactions in response
+ * @param {boolean} [comments] - Include comments in response
+ * @returns {Promise<object[]>} Redirects to login.html
+ */
+async function getPosts({
+    sort = 'created',
+    sortOrder = 'desc',
+    limit = undefined,
+    offset = undefined,
+    tag = undefined,
+    author = false,
+    reactions = false,
+    comments = false,
+}) {
+    const token = getAccessToken();
+    let queryParams = '?';
+
+    if (sort) {
+        queryParams += `sort=${sort}`;
+    }
+    if (sortOrder) {
+        queryParams += `&sortOrder=${sortOrder}`;
+    }
+    if (limit) {
+        queryParams += `&limit=${limit}`;
+    }
+    if (offset) {
+        queryParams += `&offset=${offset}`;
+    }
+    if (tag) {
+        queryParams += `&_tag=${tag}`;
+    }
+    if (author) {
+        queryParams += `&_author=true`;
+    }
+    if (reactions) {
+        queryParams += `&_reactions=true`;
+    }
+    if (comments) {
+        queryParams += `&comments=true`;
+    }
+    try {
+        const response = await fetch(baseUrl + '/posts' + queryParams, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            const error = await response.json();
+            throw new Error(`${error.statusCode} ${error.status} - ${error.errors[0].message}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        showToast('error', error);
+    }
+}
+
+export { logIn, register, isLoggedIn, getAccessToken, getPosts };
