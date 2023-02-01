b4051f124623ddffb99b25ec5b28b572f65299d1
Joakim Tveter <joakim@tveter.net>
2023-02-02 00:06:49 +0100

    Added util functions

--- scripts/utils/urlUtils.js
+++ scripts/utils/urlUtils.js
@@ -1,5 +1,8 @@
-// URL UTILITY FUNCTIONS
-
+/**
+ * Get the value of a parameter from the URL query string
+ * @param {string} parameter Url parameter to get the value from
+ * @returns {string} Value of the parameter
+ */
 function getValueFromURLParameter(parameter) {
     const urlParams = new Proxy(new URLSearchParams(window.location.search), {
         get: (searchParams, prop) => searchParams.get(prop),
@@ -7,6 +10,12 @@ function getValueFromURLParameter(parameter) {
     return urlParams[parameter];
 }
 
+/**
+ * Set a parameter and value in the URL query string
+ * @param {string} parameter
+ * @param {string} value
+ * @returns {void} Sets the value of a parameter in the URL query string
+ */
 function setUrlParameterWithoutReload(parameter = '', value = '') {
     const currentPath = window.location.pathname;
     if (parameter.length > 0 && value.length > 0) {
