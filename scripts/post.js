0747b6b372fce63b9c801a96fcd67d1f9e344d61
Joakim Tveter <joakim@tveter.net>
2023-02-06 15:37:45 +0100

    Added basic feed

---
+++ scripts/post.js
@@ -0,0 +1,62 @@
+import { timeSince } from './utils/days.js';
+
+function renderPosts(location, posts) {
+    let feed = '';
+    posts.forEach((post) => {
+        const { id, title, body, _count, media, created, updated, author } = post;
+        const { name, avatar } = author;
+        const isUpdated = created !== updated;
+        const date = new Date(created).toLocaleDateString('no-NO');
+        let singlePost = `
+            <li class="post">
+                <div class="post-header">`;
+        if (avatar) {
+            singlePost += `
+                    <img
+                        class="post-header__avatar"
+                        src="${avatar}"
+                        alt="Name profile picture"
+                        height="100px"
+                        width="100px"
+                    />`;
+        }
+        singlePost += `
+                    <div class="post-header__meta">
+                        <p class="post-header__author-name">@${name}</p>
+                        <p class="post-header__created-date">
+                            Created ${timeSince(new Date(created).getTime())} ${
+            isUpdated ? '- <span>Edited</span>' : ''
+        }</p>
+                    </div>
+                    <div class="post-header__actions">
+                        <button class="post-header__follow-button" data-author="${name}">Follow</button>
+                    </div>
+                </div>
+                <div class="post-content"> `;
+        if (media) {
+            singlePost += `
+                    <picture class="post-content__image" data-postid="${id}">
+                        <img
+                            src="${media}"
+                            alt=""
+                        />
+                    </picture>`;
+        }
+        singlePost += `
+                <h2 class="post-content__title">${title}</h2>
+                <p class="post-content__body">
+                    ${body}
+                </p>
+                </div>
+                <div class="post-footer">
+                    <button class="post-footer__like-button" data-postid="${id}">Like</button>
+                    <div class="post-footer__comments" data-postid="${id}">${_count.comments} comments</div>
+                    <div class="post-footer__reactions" data-postid="${id}">${_count.reactions} reactions</div>
+                </div>
+            </li>`;
+        feed += singlePost;
+    });
+    location.innerHTML = feed;
+}
+
+export { renderPosts };
