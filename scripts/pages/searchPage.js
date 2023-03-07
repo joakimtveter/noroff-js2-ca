e8cd1f9a472e995a1458f43684959ee906a9a234
Joakim Tveter <joakim@tveter.net>
2023-03-07 13:37:19 +0100

    Added search functionallity

--- scripts/pages/searchPage.js
+++ scripts/pages/searchPage.js
@@ -1,5 +1,5 @@
-import { getValueFromURLParameter, isLoggedIn } from '../utils.js';
-import { getPosts } from '../client.js';
+import { getValueFromURLParameter, isLoggedIn, getUserName } from '../utils.js';
+import { getPosts, getFollowingNameList } from '../client.js';
 import { renderPosts } from '../render/posts.js';
 
 // Redirect to login page if not logged in
@@ -8,21 +8,27 @@ if (!isLoggedIn()) window.location.pathname = '/login.html';
 const searchTermHeading = document.getElementById('search-term');
 const searchResultCount = document.getElementById('search-results-count');
 const searchResultsContainer = document.getElementById('search-results');
-const searchTerm = getValueFromURLParameter('q');
+const searchTerm = getValueFromURLParameter('q') || '';
+const username = getUserName();
+console.log('searchTerm:', searchTerm);
 
-if (searchTerm) {
-    const results = await getPosts({ author: true });
-    const filteredPosts = results.filter((post) => {
-        const searchTermLower = searchTerm.toLowerCase();
-        const title = post?.title.toLowerCase();
-        const author = post?.author.name.toLowerCase();
-        const body = post?.body.toLowerCase();
+const results = await getPosts({ author: true, comments: true, reactions: true });
+const followingList = await getFollowingNameList(username);
+const filteredPosts = results.filter((post) => {
+    const searchTermLower = searchTerm.toLowerCase();
+    const title = post?.title.toLowerCase();
+    const author = post?.author.name.toLowerCase();
+    const body = post?.body.toLowerCase();
+    const tags = post?.tags?.map((tag) => tag.toLowerCase());
 
-        if (title.includes(searchTermLower)) return true;
-        if (body.includes(searchTermLower)) return true;
-        if (author.includes(searchTermLower)) return true;
-    });
-    // renderPosts(searchResultsContainer, filteredPosts);
-    searchTermHeading.innerHTML = searchTerm;
-    searchResultCount.innerHTML = filteredPosts.length;
-}
+    if (title.includes(searchTermLower)) return true;
+    if (body.includes(searchTermLower)) return true;
+    if (author.includes(searchTermLower)) return true;
+    if (tags.includes(searchTermLower)) return true;
+    // return false;
+});
+searchTermHeading.innerText = searchTerm || 'All posts';
+searchResultCount.innerText = filteredPosts.length;
+document.title = `Search results for "${searchTerm || 'All posts'}" | MyPlace - Your place on the internet`;
+
+if (filteredPosts.length > 0) renderPosts(searchResultsContainer, filteredPosts, followingList);
