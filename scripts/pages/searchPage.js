62750da9ce06bce3fcdf8fae4254b17ceac32c7f
Joakim Tveter <joakim@tveter.net>
2023-03-12 23:33:19 +0100

    Tweeked loading spinner

--- scripts/pages/searchPage.js
+++ scripts/pages/searchPage.js
@@ -5,16 +5,15 @@ import { renderPosts } from '../render/posts.js';
 // Redirect to login page if not logged in
 if (!isLoggedIn()) window.location.pathname = '/login.html';
 
+const spinner = document.getElementById('loading');
 const searchTermHeading = document.getElementById('search-term');
 const searchResultCount = document.getElementById('search-results-count');
 const searchResultsContainer = document.getElementById('search-results');
 const searchField = document.getElementById('search-input');
 const searchTerm = getValueFromURLParameter('q') || '';
 const username = getUserName();
-console.log('searchTerm', searchTerm);
 
 const results = await getPosts({ author: true, comments: true, reactions: true });
-console.log('results', results);
 const followingList = await getFollowingNameList(username);
 const filteredPosts = results.filter((post) => {
     const searchTermLower = searchTerm.toLowerCase();
@@ -28,10 +27,10 @@ const filteredPosts = results.filter((post) => {
     if (author.includes(searchTermLower)) return true;
     if (tags.includes(searchTermLower)) return true;
 });
-// hideSpinner();
+
 searchField.value = searchTerm;
 searchTermHeading.innerText = searchTerm || 'All posts';
 searchResultCount.innerText = filteredPosts.length;
 document.title = `Search results for "${searchTerm || 'All posts'}" | MyPlace - Your place on the internet`;
-
+spinner.classList.add('hide-element');
 if (filteredPosts.length > 0) renderPosts(searchResultsContainer, filteredPosts, followingList);
